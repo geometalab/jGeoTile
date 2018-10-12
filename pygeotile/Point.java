b81899db8bb8d3dd28dedd28dc09af26ae4b167a
AlejoFab <35993981+AlejoFab@users.noreply.github.com>
2018-10-12 14:27:28 +0200

    Add files via upload

--- pygeotile/Point.java
+++ pygeotile/Point.java
@@ -1,3 +1,9 @@
+/**
+ * @author Alejandro Gloor
+ * @date 2018/10/12
+ * @version 1.0
+ *
+ */
 
 public class Point {
 	
@@ -7,50 +13,73 @@ public class Point {
 	private double meterY = 0.0;
 	private int pixelX = 0;
 	private int pixelY = 0;
-	private int zoom = 0;
 	
-//	/**
-//	 * Creates a point from lat/lon in WGS84
-//	 * @return 
-//	 */
-//	public double fromLatitudeLongitude(double latitude, double longitude) {
-//        assert -180.0 <= longitude <= 180.0; // Longitude needs to be a value between -180.0 and 180.0.
-//        assert -90.0 <= latitude <= 90.0; //Latitude needs to be a value between -90.0 and 90.0.
-//        this.latitude = latitude;
-//        this.longitude = longitude;
-//        return latitude, longitude;
-//	}
-//	
-//	/**
-//	 * Creates a point from pixels X Y Z (zoom) in pyramid
-//	 * @return 
-//	 */
-//	public int fromPixel(int pixelX, int pixelY, int zoom) {
-//        int maxPixel = (Math.pow(2,zoom)) * TILE_SIZE;
-//        assert 0 <= pixelX <= max_pixel, //Point X needs to be a value between 0 and (2^zoom) * 256.
-//        assert 0 <= pixelY <= max_pixel, //Point Y needs to be a value between 0 and (2^zoom) * 256.
-//        meterX = pixelX * resolution(zoom) - ORIGIN_SHIFT;
-//        meterY = pixelY * resolution(zoom) - ORIGIN_SHIFT;
-//        meterX, meterY = cls._sign_meters(meters=(meterX, meterY), pixels=(pixelX, pixelY), zoom=zoom);
-//        return cls.from_meters(meterX=meterX, meterY=meterY);
-//	}
-//	
-//	/**
-//	 * Creates a point from X Y Z (zoom) meters in Spherical Mercator EPSG:900913
-//	 * @return 
-//	 */
-//	public double fromMeters(double meterX, double meterY) {
-//        assert -ORIGIN_SHIFT <= meter_x <= ORIGIN_SHIFT, \
-//            'Meter X needs to be a value between -{0} and {0}.'.format(ORIGIN_SHIFT);
-//        assert -ORIGIN_SHIFT <= meter_y <= ORIGIN_SHIFT, \
-//            'Meter Y needs to be a value between -{0} and {0}.'.format(ORIGIN_SHIFT);
-//        longitude = (meterX / ORIGIN_SHIFT) * 180.0;
-//        latitude = (meterY / ORIGIN_SHIFT) * 180.0;
-//        latitude = 180.0 / Math.PI * (2 * Math.atan(Math.exp(latitude * Math.PI / 180.0)) - Math.PI / 2.0);
-//        this.latitude = latitude;
-//        this.longitude = longitude;
-//        return latitude, longitude;
-//	}
+	/**
+	 * Constructor
+	 * @param latitude
+	 * @param longitude 
+	 */
+	public Point(double latitude, double longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	/**
+	 * Creates a point from latitude/longitude in WGS84
+	 * @param latitude
+	 * @param longitude
+	 * @return Point
+	 */
+	public Point fromLatitudeLongitude(double latitude, double longitude) {
+        if(-180.0 > longitude && longitude > 180.0) {
+        	System.out.println("Longitude needs to be a value between -180.0 and 180.0.");
+        }
+        if(-90.0 > latitude && latitude > 90.0) {
+        	System.out.println("Latitude needs to be a value between -90.0 and 90.0.");
+        }
+		return new Point(latitude, longitude);
+	}
+	
+	/**
+	 * Creates a point from pixels X Y Z (zoom) in pyramid
+	 * @param pixelX
+	 * @param pixelY
+	 * @param zoom
+	 * @return Point
+	 */
+	public Point fromPixel(int pixelX, int pixelY, int zoom) {
+		int maxPixel = (int)((Math.pow(2,zoom)) * Meta.TILE_SIZE);
+		if(0 > pixelX && pixelX > maxPixel) {
+			System.out.println("Point X needs to be a value between 0 and (2^zoom) * 256.");
+		}
+		if(0 > pixelY && pixelY > maxPixel) {
+			System.out.println("Point Y needs to be a value between 0 and (2^zoom) * 256.");
+		}
+		meterX = pixelX * Meta.resolution(zoom) - Meta.ORIGIN_SHIFT;
+		meterY = pixelY * Meta.resolution(zoom) - Meta.ORIGIN_SHIFT;
+		meterX = signMeterX(meterX, pixelX, zoom);
+		meterY = signMeterY(meterY, pixelY, zoom);
+		return fromMeters(meterX, meterY);
+	}
+	
+	/**
+	 * Creates a point from X Y Z (zoom) meters in Spherical Mercator EPSG:900913
+	 * @param meterX
+	 * @param meterY
+	 * @return Point
+	 */
+	public Point fromMeters(double meterX, double meterY) {
+	    if(-Meta.ORIGIN_SHIFT > meterX && meterX > Meta.ORIGIN_SHIFT) {
+	        System.out.println("Meter X needs to be a value between " + -Meta.ORIGIN_SHIFT + " and " + Meta.ORIGIN_SHIFT + ".");
+	    }
+	    if(-Meta.ORIGIN_SHIFT > meterY && meterY > Meta.ORIGIN_SHIFT) {
+	       System.out.println("Meter Y needs to be a value between " + -Meta.ORIGIN_SHIFT + " and " + Meta.ORIGIN_SHIFT + ".");
+	    }
+	    longitude = (meterX / Meta.ORIGIN_SHIFT) * 180.0;
+	    latitude = (meterY / Meta.ORIGIN_SHIFT) * 180.0;
+	    latitude = 180.0 / Math.PI * (2 * Math.atan(Math.exp(latitude * Math.PI / 180.0)) - Math.PI / 2.0);
+		return new Point(latitude, longitude);
+	}
 	
 	/**
 	 * Gets latitude in WGS84
@@ -70,20 +99,20 @@ public class Point {
 	
 	/**
 	 * Gets pixel X of the EPSG:4326 pyramid by a specific zoom, converted from lat/lon in WGS84
+	 * @param zoom
 	 * @return pixelX
 	 */
 	public int getPixelX(int zoom) {
-        meterX = getMeterX();
         pixelX = (int)((meterX + Meta.ORIGIN_SHIFT) / Meta.resolution(zoom));
         return Math.abs(Math.round(pixelX));
 	}
 	
 	/**
 	 * Gets pixel Y of the EPSG:4326 pyramid by a specific zoom, converted from lat/lon in WGS84
+	 * @param zoom
 	 * @return pixelY
 	 */
 	public int getPixelY(int zoom) {
-        meterY = getMeterY();
         pixelY = (int)((meterY - Meta.ORIGIN_SHIFT) / Meta.resolution(zoom));
         return Math.abs(Math.round(pixelY));
 	}
@@ -93,7 +122,6 @@ public class Point {
 	 * @return meterX
 	 */
 	public double getMeterX() {
-        longitude = getLongitude();
         meterX = longitude * Meta.ORIGIN_SHIFT / 180.0;
         return meterX;
 	}
@@ -103,17 +131,19 @@ public class Point {
 	 * @return meterY
 	 */
 	public double getMeterY() {
-        latitude = getLatitude();
         meterY = Math.log(Math.tan((90.0 + latitude) * Math.PI / 360.0)) / (Math.PI / 180.0);
         meterY = meterY * Meta.ORIGIN_SHIFT / 180.0;
         return meterY;
 	}
 	
 	/**
-	 * 
+	 * Checks if meter X ist bigger than halfsize
+	 * @param meterX
+	 * @param pixelX
+	 * @param zoom
 	 * @return meterX
 	 */
-	public double signMeterX(double meterX, double meterY, int pixelX, int pixelY, int zoom) {
+	public double signMeterX(double meterX, int pixelX, int zoom) {
 		int halfSize = (int)((Meta.TILE_SIZE * Math.pow(2,zoom)) / 2);
 		this.pixelX = pixelX;
 		this.meterX = meterX;
@@ -125,10 +155,13 @@ public class Point {
 	}
 	
 	/**
-	 * 
+	 * Checks if meter Y ist bigger than halfsize
+	 * @param meterY
+	 * @param pixelY
+	 * @param zoom
 	 * @return meterY
 	 */
-	public double signMeterY(double meterX, double meterY, int pixelX, int pixelY, int zoom) {
+	public double signMeterY(double meterY, int pixelY, int zoom) {
 		int halfSize = (int)((Meta.TILE_SIZE * Math.pow(2,zoom)) / 2);
 		this.pixelY = pixelY;
 		this.meterY = meterY;
