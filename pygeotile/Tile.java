6cd7d15a6f777b52c745f736a38e92e951dbb373
SeeBenj <44023979+SeeBenj@users.noreply.github.com>
2018-10-12 13:10:41 +0200

    Bounds Methode eingefüght

--- pygeotile/Tile.java
+++ pygeotile/Tile.java
@@ -8,12 +8,24 @@ public class Tile {
 	int tmsY;
 	float tileSize = Meta.TILE_SIZE;
 
+	/**
+	 * Constructor for Tile
+	 * @param longitude
+	 * @param latitude
+	 * @param zoom
+	 */
 	private Tile(double longitude, double latitude, int zoom) {
 		longitude = this.tmsX;
 		latitude = this.tmsY;
 		zoom = this.zoom;
 	}
 
+	/**
+	 * 
+	 * @param quadTree
+	 * @return
+	 * @throws Exception
+	 */
 	public Tile fromQuadTree(String quadTree) throws Exception {
 		// """Creates a tile from a Microsoft QuadTree"""
 		zoom = quadTree.length();
@@ -46,6 +58,13 @@ public class Tile {
 		return new Tile(tmsX, tmsY, zoom);
 	}
 
+	/**
+	 * 
+	 * @param zoom
+	 * @param tmsX
+	 * @param tmsY
+	 * @return
+	 */
 	public Tile fromTms(int zoom, int tmsX, int tmsY) {
 		// """Creates a tile from Tile Map Service (TMS) X Y and zoom"""
 		int max_tile = (2 * zoom) - 1;
@@ -60,6 +79,13 @@ public class Tile {
 		return new Tile(tmsX, tmsY, zoom);
 	}
 
+	/**
+	 * 
+	 * @param googleX
+	 * @param googleY
+	 * @param zoom
+	 * @return
+	 */
 	public Tile fromGoogle(int googleX, int googleY, int zoom) {
 		// """Creates a tile from Google format X Y and zoom"""
 		int max_tile = (2 * zoom) - 1;
@@ -74,6 +100,12 @@ public class Tile {
 		return new Tile(tmsX, tmsY, zoom);
 	}
 
+	/**
+	 * 
+	 * @param point
+	 * @param zoom
+	 * @return
+	 */
 	public Tile forPoint(Point point, int zoom) {
 		// """Creates a tile for given point"""
 		double latitude = point.getLatitude();
@@ -81,6 +113,13 @@ public class Tile {
 		return new Tile(longitude,latitude, zoom);
 	}
 
+	/**
+	 * 
+	 * @param pixelX
+	 * @param pixelY
+	 * @param zoom
+	 * @return
+	 */
 	public Tile forPixels(int pixelX, int pixelY, int zoom) {
 		// """Creates a tile from pixels X Y Z (zoom) in pyramid"""
 		tmsX = (int) (Math.ceil(pixelX / tileSize)-1);
@@ -89,6 +128,13 @@ public class Tile {
 		return new Tile(tmsX, tmsY, zoom);
 	}
 
+	/**
+	 * 
+	 * @param meterX
+	 * @param meterY
+	 * @param zoom
+	 * @return
+	 */
 	public Tile forMeters(double meterX, double meterY, int zoom) {
 		// """Creates a tile from X Y meters in Spherical Mercator EPSG:900913"""
 		Point point = Point.fromMeters(meterX, meterY);
@@ -97,6 +143,13 @@ public class Tile {
 		return forPixels(pixelX2,pixelY2,zoom);
 	}
 
+	/**
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @param zoom
+	 * @return
+	 */
 	public Tile forLatitudeLongitude(double latitude, double longitude, int zoom) {
 		// """Creates a tile from lat/lon in WGS84"""
 		Point point = Point.fromLatitudeLongitude(latitude, longitude);
@@ -104,12 +157,21 @@ public class Tile {
 		int pixelY = point.getPixelY(zoom);
 		return forPixels(pixelX,pixelY,zoom);
 	}
+	
+	/**
+	 * 
+	 * @return retunrs an Int array containing the X and Y values from this TMS
+	 */
 	public int[] getTms() {
 		//gives out tmsX and tmsY in an int array
 		int [] tms = {tmsX,tmsY};
 		return tms;
 	}
-
+	
+	/**
+	 * 
+	 * @return Returns the QuadTree String converted from TMS
+	 */
 	public String quadTree() {
 		// """Gets the tile in the Microsoft QuadTree format, converted from TMS"""
 		StringBuilder quadKey = new StringBuilder();
@@ -128,16 +190,35 @@ public class Tile {
 		return quadKey.toString();
 
 	}
-
+	/**
+	 * 
+	 * @return An int Array containing the X and Y values for the Google format, converted from TMS
+	 */
 	public int[] getGoogle() {
 		// """Gets the tile in the Google format, converted from TMS"""
 		int[] google = {tmsX, (2*zoom-1)-tmsY};
 		return google;
 	}
-
-	public void bounds() {
+	
+	/**
+	 * 
+	 * @return A Point Array that containst bottom right and the top Left point of the Tile.
+	 * 
+	 */
+	public Point[] bounds() {
 		// """Gets the bounds of a tile represented as the most west and south point and
 		// the most east and north point"""
+		int googleX = getGoogle()[0];
+		int googleY = getGoogle()[1];
+		double pixelXWest = googleX * tileSize;
+		double pixelYNorth = googleY * tileSize;
+		double pixelXEast = (googleX+1) * tileSize;
+		double pixelYSouth = (googleY+1) * tileSize;
+		
+		Point pointMin = Point.fromPixel(pixelXWest, pixelYSouth, zoom);
+		Point pointMax = Point.fromPixel(pixelXEast, pixelYNorth,zoom);
+		Point[] bounds = {pointMin, pointMax};
+		return bounds;
 
 	}
 }
