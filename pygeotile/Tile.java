9fa978087cb45de27751a1e6a897514c3c48a7e3
SeeBenj <44023979+SeeBenj@users.noreply.github.com>
2018-10-12 09:44:48 +0200

    Variablen Namen angepasst

--- pygeotile/Tile.java
+++ pygeotile/Tile.java
@@ -4,22 +4,20 @@ import java.lang.Math;
 
 public class Tile {
 	int zoom;
-	int tms_x;
-	int tms_y;
+	int tmsX;
+	int tmsY;
 
 	private Tile(int tms_x, int tms_y, int zoom) {
-		tms_x = this.tms_x;
-		tms_y = this.tms_y;
+		tms_x = this.tmsX;
+		tms_y = this.tmsY;
 		zoom = this.zoom;
 	}
 
 	public Tile fromQuadTree(String quadTree) throws Exception {
 		// """Creates a tile from a Microsoft QuadTree"""
 		zoom = quadTree.length();
-
-		double offset = (Math.pow(2, zoom)) - 1;
-		int google_x = 0;
-		int google_y = 0;
+		int googleX = 0;
+		int googleY = 0;
 
 		for (int i = zoom; i > 0; --i) {
 			int mask = 1 << (i - 1);
@@ -28,60 +26,52 @@ public class Tile {
 				break;
 
 			case '1':
-				google_x |= mask;
+				googleX |= mask;
 
 			case '2':
-				google_y |= mask;
+				googleY |= mask;
 
 			case '3':
-				google_x |= mask;
-				google_y |= mask;
+				googleX |= mask;
+				googleY |= mask;
 
 			default:
 				throw new Exception("QuadTree value can only consists of the digits 0, 1, 2 and 3.");
 			}
 		}
-		tms_x = google_x;
-		tms_y = google_y;
+		tmsX = googleX;
+		tmsY = googleY;
 
-		return new Tile(tms_x, tms_y, zoom);
+		return new Tile(tmsX, tmsY, zoom);
 	}
 
-	public Tile fromTms(int zoom, int tms_x, int tms_y) {
+	public Tile fromTms(int zoom, int tmsX, int tmsY) {
 		// """Creates a tile from Tile Map Service (TMS) X Y and zoom"""
 		int max_tile = (2 * zoom) - 1;
-		// assert 0 <= tms_x <= max_tile, 'TMS X needs to be a value between 0 and
-		// (2^zoom) -1.';
-		// assert 0 <= tms_y <= max_tile, 'TMS Y needs to be a value between 0 and
-		// (2^zoom) -1.';
-		// Java
-		if (0 > tms_x || tms_x > max_tile) {
+		if (0 > tmsX || tmsX > max_tile) {
 			System.out.println("TMS X needs to be a value between 0 and (2^zoom) -1.");
 		}
 
-		if (0 > tms_y || tms_y > max_tile) {
+		if (0 > tmsY || tmsY > max_tile) {
 			System.out.println("TMS Y needs to be a value between 0 and (2^zoom) -1.");
 		}
 		//
-		return new Tile(tms_x, tms_y, zoom);
+		return new Tile(tmsX, tmsY, zoom);
 	}
 
-	public Tile fromGoogle(int google_x,int google_y,int zoom) {
-			//"""Creates a tile from Google format X Y and zoom"""
-	        int max_tile = (2 * zoom) - 1;
-	        //assert 0 <= google_x <= max_tile, 'Google X needs to be a value between 0 and (2^zoom) -1.'
-	        //assert 0 <= google_y <= max_tile, 'Google Y needs to be a value between 0 and (2^zoom) -1.'
-	        //Java
-	        if(0 > google_x || google_x > max_tile) {
-	        	System.out.println("Google X needs to be a value between 0 and (2^zoom) -1.");
-	        }
-	        if(0 > google_y || google_y > max_tile) {
-	        	System.out.println("Google Y needs to be a value between 0 and (2^zoom) -1.");
-	        }
-			tms_x = google_x;
-			tms_y = (2*zoom-1)-google_y;
-			return new Tile(tms_x, tms_y, zoom);
+	public Tile fromGoogle(int googleX, int googleY, int zoom) {
+		// """Creates a tile from Google format X Y and zoom"""
+		int max_tile = (2 * zoom) - 1;
+		if (0 > googleX || googleX > max_tile) {
+			System.out.println("Google X needs to be a value between 0 and (2^zoom) -1.");
 		}
+		if (0 > googleY || googleY > max_tile) {
+			System.out.println("Google Y needs to be a value between 0 and (2^zoom) -1.");
+		}
+		tmsX = googleX;
+		tmsY = (2 * zoom - 1) - googleY;
+		return new Tile(tmsX, tmsY, zoom);
+	}
 
 	public void forPoint(int point, int zoom) {
 		// """Creates a tile for given point"""
@@ -99,10 +89,12 @@ public class Tile {
 		// """Creates a tile from lat/lon in WGS84"""
 	}
 
-	public void getTmsX() {
+	public int getTmsX() {
+		return tmsX;
 	}
 
-	public void getTmsY() {
+	public int getTmsY() {
+		return tmsY;
 	}
 
 	public void quadTree() {
