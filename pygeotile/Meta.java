526b822cbfb8d5c7e647f85b68c257385f1ef234
AlejoFab <35993981+AlejoFab@users.noreply.github.com>
2018-10-12 08:13:33 +0200

    Add files via upload

--- pygeotile/Meta.java
+++ pygeotile/Meta.java
@@ -1,12 +1,12 @@
 
 public class Meta {
 
-	private final double EARTH_RADIUS = 6378137.0;
-	public final int TILE_SIZE = 256;
-	public final double ORIGIN_SHIFT = 2.0 * Math.PI * EARTH_RADIUS / 2.0;
-	private final double INITIAL_RESOLUTION = 2.0 * Math.PI * EARTH_RADIUS / (float)TILE_SIZE;
+	private static final double EARTH_RADIUS = 6378137.0;
+	public static final int TILE_SIZE = 256;
+	public static final double ORIGIN_SHIFT = 2.0 * Math.PI * EARTH_RADIUS / 2.0;
+	private static final double INITIAL_RESOLUTION = 2.0 * Math.PI * EARTH_RADIUS / (float)TILE_SIZE;
 	
-	public double resolution(int zoom) {
+	public static double resolution(int zoom) {
 		return INITIAL_RESOLUTION / (Math.pow(2,zoom));
 	}
 }
