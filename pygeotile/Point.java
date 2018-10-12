bdfa569dd6b4b293b35c3b572999e1fef99bef19
AlejoFab <35993981+AlejoFab@users.noreply.github.com>
2018-10-12 16:11:20 +0200

    Add files via upload
    Assert Statements have been added

--- pygeotile/Point.java
+++ pygeotile/Point.java
@@ -29,9 +29,8 @@ public class Point {
 	public static Point fromLatitudeLongitude(double latitude, double longitude) {
         assert -180.0 > longitude && longitude > 180.0 :
         	"Longitude needs to be a value between -180.0 and 180.0.";
-        if(-90.0 > latitude && latitude > 90.0) {
-        	System.out.println("Latitude needs to be a value between -90.0 and 90.0.");
-        }
+        assert -90.0 > latitude && latitude > 90.0 :
+        	"Latitude needs to be a value between -90.0 and 90.0.";
 		return new Point(latitude, longitude);
 	}
 	
@@ -44,12 +43,10 @@ public class Point {
 	 */
 	public static Point fromPixel(int pixelX, int pixelY, int zoom) {
 		int maxPixel = (int)((Math.pow(2,zoom)) * Meta.TILE_SIZE);
-		if(0 > pixelX && pixelX > maxPixel) {
-			System.out.println("Point X needs to be a value between 0 and (2^zoom) * 256.");
-		}
-		if(0 > pixelY && pixelY > maxPixel) {
-			System.out.println("Point Y needs to be a value between 0 and (2^zoom) * 256.");
-		}
+		assert 0 > pixelX && pixelX > maxPixel :
+			"Point X needs to be a value between 0 and (2^zoom) * 256.";
+		assert 0 > pixelY && pixelY > maxPixel :
+			"Point Y needs to be a value between 0 and (2^zoom) * 256.";
 		double meterX = pixelX * Meta.resolution(zoom) - Meta.ORIGIN_SHIFT;
 		double meterY = pixelY * Meta.resolution(zoom) - Meta.ORIGIN_SHIFT;
 		meterX = signMeterX(meterX, pixelX, zoom);
@@ -64,12 +61,10 @@ public class Point {
 	 * @return Point
 	 */
 	public static Point fromMeters(double meterX, double meterY) {
-	    if(-Meta.ORIGIN_SHIFT > meterX && meterX > Meta.ORIGIN_SHIFT) {
-	        System.out.println("Meter X needs to be a value between " + -Meta.ORIGIN_SHIFT + " and " + Meta.ORIGIN_SHIFT + ".");
-	    }
-	    if(-Meta.ORIGIN_SHIFT > meterY && meterY > Meta.ORIGIN_SHIFT) {
-	       System.out.println("Meter Y needs to be a value between " + -Meta.ORIGIN_SHIFT + " and " + Meta.ORIGIN_SHIFT + ".");
-	    }
+	    assert -Meta.ORIGIN_SHIFT > meterX && meterX > Meta.ORIGIN_SHIFT :
+	        "Meter X needs to be a value between " + -Meta.ORIGIN_SHIFT + " and " + Meta.ORIGIN_SHIFT + ".";
+	    assert -Meta.ORIGIN_SHIFT > meterY && meterY > Meta.ORIGIN_SHIFT :
+	       "Meter Y needs to be a value between " + -Meta.ORIGIN_SHIFT + " and " + Meta.ORIGIN_SHIFT + ".";
 	    double longitude = (meterX / Meta.ORIGIN_SHIFT) * 180.0;
 	    double  latitude = (meterY / Meta.ORIGIN_SHIFT) * 180.0;
 	    latitude = 180.0 / Math.PI * (2 * Math.atan(Math.exp(latitude * Math.PI / 180.0)) - Math.PI / 2.0);
