9ffb747dd4a8e4db5a0549e00aca4b6ee418d49b
Gloor Alejandro <agloor@hsr.ch>
2018-10-12 16:30:18 +0200

    Initial commit of test class for point class

---
+++ tests/PointTest.java
@@ -0,0 +1,59 @@
+import org.junit.jupiter.api.Test;
+
+public class PointTest {
+
+	@Test
+	public void testFromLatitudeLongitude() {
+		
+	}
+	
+	@Test
+	public void testFromPixel() {
+		
+	}
+	
+	@Test
+	public void testFromMeters() {
+		
+	}
+	
+	@Test
+	public void testGetLatitude() {
+		
+	}
+	
+	@Test
+	public void testGetLongitude() {
+		
+	}
+	
+	@Test
+	public void testGetPixelX() {
+		
+	}
+	
+	@Test
+	public void testGetPixelY() {
+		
+	}
+	
+	@Test
+	public void testGetMeterX() {
+		
+	}
+	
+	@Test
+	public void testGetMeterY() {
+		
+	}
+	
+	@Test
+	public void testSignMeterX() {
+		
+	}
+	
+	@Test
+	public void testSignMeterY() {
+		
+	}
+}
