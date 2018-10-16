7389211824557b0f113447f67554049e35c87677
Gloor Alejandro <agloor@hsr.ch>
2018-10-16 15:42:24 +0200

    Included more test cases

--- tests/PointTest.java
+++ tests/PointTest.java
@@ -8,6 +8,7 @@ public class PointTest {
 	double longitude = -87.65;
 	double meterX = -9757148.442088600;
 	double meterY = 5138517.444985110;
+	double epsilon = 0.001;
 	int pixelX = 34430575;
 	int pixelY = 49899071;
 	int zoom = 19;
@@ -15,17 +16,17 @@ public class PointTest {
 	@Test
 	public void testFromLatitudeLongitude() {
 		Point point = Point.fromLatitudeLongitude(latitude, longitude);
-		assertEquals(latitude, point.getLatitude(), 0.001);
-		assertEquals(longitude, point.getLongitude(), 0.001);
+		assertEquals(latitude, point.getLatitude(), epsilon);
+		assertEquals(longitude, point.getLongitude(), epsilon);
 	}
 	
 	@Test
 	public void testFromMeters() {
 		Point point = Point.fromMeters(meterX, meterY);
-		assertEquals(meterX, point.getMeterX(), 0.001);
-		assertEquals(meterY, point.getMeterY(), 0.001);
-		assertEquals(latitude, point.getLatitude(), 0.001);
-		assertEquals(longitude, point.getLongitude(), 0.001);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		assertEquals(latitude, point.getLatitude(), epsilon);
+		assertEquals(longitude, point.getLongitude(), epsilon);
 	}
 	
 	@Test
@@ -33,18 +34,18 @@ public class PointTest {
 		Point point = Point.fromPixel(pixelX, pixelY, zoom);
 		assertEquals(pixelX, point.getPixelX(zoom));
 		assertEquals(pixelY, point.getPixelY(zoom));
-		assertEquals(latitude, point.getLatitude(), 0.001);
-		assertEquals(longitude, point.getLongitude(), 0.001);
+		assertEquals(latitude, point.getLatitude(), epsilon);
+		assertEquals(longitude, point.getLongitude(), epsilon);
 	}
 	
 	@Test
 	public void testGetLatitude() {
-		assertEquals(latitude, 41.85, 0.000001);
+		assertEquals(latitude, 41.85, epsilon);
 	}
 	
 	@Test
 	public void testGetLongitude() {
-		assertEquals(longitude, -87.65, 0.000001);
+		assertEquals(longitude, -87.65, epsilon);
 	}
 	
 	@Test
@@ -59,25 +60,102 @@ public class PointTest {
 	
 	@Test
 	public void testGetMeterX() {
-		assertEquals(meterX, -9757148.442088600, 0.00000001);
+		assertEquals(meterX, -9757148.442088600, epsilon);
 	}
 	
 	@Test
 	public void testGetMeterY() {
-		assertEquals(meterY, 5138517.444985110, 0.00000001);
+		assertEquals(meterY, 5138517.444985110, epsilon);
 	}
 	
 	@Test
 	public void testSignMeterX() {
-		Point.signMeterX(meterX, pixelX, zoom);
-		assertEquals(-9757148.442088600, meterX, 0.00000001);
-		System.out.println("testSignMeterX: " + Point.signMeterX(meterX, pixelX, zoom));
+		Point point  = new Point(latitude, longitude);
+		assertEquals(meterX, Point.signMeterX(meterX, pixelX, zoom), epsilon);
+		assertEquals(latitude, point.getLatitude(), epsilon);
+		assertEquals(longitude, point.getLongitude(), epsilon);
 	}
 	
 	@Test
 	public void testSignMeterY() {
-		Point.signMeterY(meterY, pixelY, zoom);
-		assertEquals(5138517.444985110, meterY, 0.00000001);
-		System.out.println("testSignMeterY: " + Point.signMeterX(meterX, pixelX, zoom));
+		Point point  = new Point(latitude, longitude);
+		assertEquals(meterY, Point.signMeterY(meterY, pixelY, zoom), epsilon);
+		assertEquals(latitude, point.getLatitude(), epsilon);
+		assertEquals(longitude, point.getLongitude(), epsilon);
+	}
+	
+	@Test
+	public void testPixelsToMeters() {
+		
+	}
+	
+	@Test
+	public void testPixelsToLatitudeLongitude() {
+		
+	}
+	
+	@Test
+	public void testMetersToPixels() {
+		
+	}
+	
+	@Test
+	public void testAssertLongitude() {
+		
+	}
+	
+	@Test
+	public void testNoAssertLongitude() {
+		
+	}
+	
+	@Test
+	public void testAssertLatitude() {
+		
+	}
+	
+	@Test
+	public void testNoAssertLatitude() {
+		
+	}
+	
+	@Test
+	public void testAssertPixelX() {
+		
+	}
+	
+	@Test
+	public void testNoAssertPixelX() {
+		
+	}
+	
+	@Test
+	public void testAssertPixelY() {
+		
+	}
+	
+	@Test
+	public void testNoAssertPixelY() {
+		
+	}
+	
+	@Test
+	public void testAssertMeterX() {
+		
+	}
+	
+	@Test
+	public void testNoAssertMeterX() {
+		
+	}
+	
+	@Test
+	public void testAssertMeterY() {
+		
+	}
+	
+	@Test
+	public void testNoAssertMeterY() {
+		
 	}
 }
