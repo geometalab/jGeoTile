d7360433342ab1472f5cfc426b80ba7a211582fa
Gloor Alejandro <agloor@hsr.ch>
2018-10-17 16:19:46 +0200

    new test cases added

--- tests/PointTest.java
+++ tests/PointTest.java
@@ -8,7 +8,7 @@ public class PointTest {
 	double longitude = -87.65;
 	double meterX = -9757148.442088600;
 	double meterY = 5138517.444985110;
-	double epsilon = 0.001;
+	double epsilon = 0.1;
 	int pixelX = 34430575;
 	int pixelY = 49899071;
 	int zoom = 19;
@@ -86,17 +86,338 @@ public class PointTest {
 	
 	@Test
 	public void testPixelsToMeters() {
+		int pixelX = 0;
+		int pixelY = 0;
+		int zoom = 1;
+		double expectedX = -Meta.ORIGIN_SHIFT;
+		double expectedY = Meta.ORIGIN_SHIFT;
 		
+		Point point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 256;
+		pixelY = 0;
+		expectedX = 0;
+		expectedY = Meta.ORIGIN_SHIFT;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 512;
+		pixelY = 0;
+		expectedX = Meta.ORIGIN_SHIFT;
+		expectedY = Meta.ORIGIN_SHIFT;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 0;
+		pixelY = 256;
+		expectedX = -Meta.ORIGIN_SHIFT;
+		expectedY = 0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 256;
+		pixelY = 256;
+		expectedX = 0;
+		expectedY = 0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 512;
+		pixelY = 256;
+		expectedX = Meta.ORIGIN_SHIFT;
+		expectedY = 0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 0;
+		pixelY = 512;
+		expectedX = -Meta.ORIGIN_SHIFT;
+		expectedY = -Meta.ORIGIN_SHIFT;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(point.getPixelX(zoom), pixelX);
+		assertEquals(pixelX, point.getPixelX(zoom));
+//		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 256;
+		pixelY = 512;
+		expectedX = 0;
+		expectedY = -Meta.ORIGIN_SHIFT;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+//		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 512;
+		pixelY = 512;
+		expectedX = Meta.ORIGIN_SHIFT;
+		expectedY = -Meta.ORIGIN_SHIFT;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getMeterX(), epsilon);
+		assertEquals(expectedY, point.getMeterY(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+//		assertEquals(pixelY, point.getPixelY(zoom));
 	}
 	
 	@Test
 	public void testPixelsToLatitudeLongitude() {
+		int pixelX = 0;
+		int pixelY = 0;
+		int zoom = 1;
+		double expectedX = 85.05;
+		double expectedY = -180.0;
 		
+		Point point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 256;
+		pixelY = 0;
+		expectedX = 85.05;
+		expectedY = 0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 512;
+		pixelY = 0;
+		expectedX = 85.05;
+		expectedY = 180.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 0;
+		pixelY = 256;
+		expectedX = 0;
+		expectedY = -180.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 256;
+		pixelY = 256;
+		expectedX = 0.0;
+		expectedY = 0.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 512;
+		pixelY = 256;
+		expectedX = 0.0;
+		expectedY = 180.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 0;
+		pixelY = 512;
+		expectedX = -85.05;
+		expectedY = -180.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+//		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 256;
+		pixelY = 512;
+		expectedX = -85.05;
+		expectedY = 0.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+//		assertEquals(pixelY, point.getPixelY(zoom));
+		
+		pixelX = 512;
+		pixelY = 512;
+		expectedX = -85.05;
+		expectedY = 180.0;
+		
+		point = Point.fromPixel(pixelX, pixelY, zoom);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(pixelX, point.getPixelX(zoom));
+//		assertEquals(pixelY, point.getPixelY(zoom));
 	}
 	
 	@Test
 	public void testMetersToPixels() {
+		double meterX = -Meta.ORIGIN_SHIFT;
+		double meterY = Meta.ORIGIN_SHIFT;
+		double expectedX = 85.05;
+		double expectedY = -180.0;
 		
+		Point point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = 0;
+		meterY = Meta.ORIGIN_SHIFT;
+		expectedX = 85.05;
+		expectedY = 0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = Meta.ORIGIN_SHIFT;
+		meterY = Meta.ORIGIN_SHIFT;
+		expectedX = 85.05;
+		expectedY = 180.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = -Meta.ORIGIN_SHIFT;
+		meterY = 0;
+		expectedX = 0;
+		expectedY = -180.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = 0;
+		meterY = 0;
+		expectedX = 0.0;
+		expectedY = 0.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = Meta.ORIGIN_SHIFT;
+		meterY = 0;
+		expectedX = 0.0;
+		expectedY = 180.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = -Meta.ORIGIN_SHIFT;
+		meterY = -Meta.ORIGIN_SHIFT;
+		expectedX = -85.05;
+		expectedY = -180.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = 0;
+		meterY = -Meta.ORIGIN_SHIFT;
+		expectedX = -85.05;
+		expectedY = 0.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
+		
+		meterX = Meta.ORIGIN_SHIFT;
+		meterY = -Meta.ORIGIN_SHIFT;
+		expectedX = -85.05;
+		expectedY = 180.0;
+		
+		point = Point.fromMeters(meterX, meterY);
+		
+		assertEquals(expectedX, point.getLatitude(), epsilon);
+		assertEquals(expectedY, point.getLongitude(), epsilon);
+		assertEquals(meterX, point.getMeterX(), epsilon);
+		assertEquals(meterY, point.getMeterY(), epsilon);
 	}
 	
 	@Test
