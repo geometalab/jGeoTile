b4ff33f3333f823881a71f1819bd2987c24c8e47
Mario Meili <mario.meili@live.com>
2018-10-17 15:46:21 +0200

    tests: add test suite to run all test classes

---
+++ tests/TestSuite.java
@@ -0,0 +1,9 @@
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({ PointTest.class, TileTest.class })
+public class TestSuite {
+}
