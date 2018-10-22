aed2e5d22c1e519a904246f37a465355fd29da2e
Gloor Alejandro <agloor@hsr.ch>
2018-10-22 16:06:55 +0200

    new package names

--- tests/TestSuite.java
+++
@@ -1,9 +0,0 @@
-
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.platform.suite.api.SelectClasses;
-import org.junit.runner.RunWith;
-
-@RunWith(JUnitPlatform.class)
-@SelectClasses({ PointTest.class, TileTest.class })
-public class TestSuite {
-}
