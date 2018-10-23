61214fffd2ecc9dfd86431a42a2ff8612cd38ede
Mario Meili <mario.meili@live.com>
2018-10-23 13:28:30 +0200

    tests: refactor and complete tile tests

--- tests/ch/hsr/ifs/jgeotile/Chicago.java
+++ tests/ch/hsr/ifs/jgeotile/Chicago.java
@@ -9,14 +9,38 @@ package ch.hsr.ifs.jgeotile;
  */
 class Chicago {
 
-	public static double latitude = 41.85;
-	public static double longitude = -87.65;
 	public static int zoom = 19;
+	
+	public static final double latitude = 41.85;
+	public static final double longitude = -87.65;
+	
+	public static final double lowerBoundLatitude = 41.8496161693754;
+	public static final double lowerBoundLongitude = -87.65029907226562;
+	public static final double upperBoundLatitude = 41.85012764855732;
+	public static final double upperBoundLongitude = -87.64961242675781;
 
-	public static int pixelX = 34430575;
-	public static int pixelY = 49899071;
+	public static final int pixelX = 34430575;
+	public static final int pixelY = 49899071;
+	
+	public static final int lowerPixelBoundX = 34430464;
+	public static final int lowerPixelBoundY = 49899264;
+	public static final int upperPixelBoundX = 34430720;
+	public static final int upperPixelBoundY = 49899008;
 
-	public static double meterX = -9757148.442088600;
-	public static double meterY = 5138517.444985110;
-
+	public static final double meterX = -9757148.442088600;
+	public static final double meterY = 5138517.444985110;
+	
+	public static final double lowerMeterBoundX = -9757186.660602748;
+	public static final double lowerMeterBoundY = 5138479.226470973;
+	public static final double upperMeterBoundX = -9757110.223574463;
+	public static final double upperMeterBoundY = 5138555.663499258;
+	
+	public static final int googleX = 134494;
+	public static final int googleY = 194918;
+	
+	public static final int tmsX = 134494;
+	public static final int tmsY = 329369;
+	
+	public static final String quadTree = "0302222310303211330";
+	
 }
